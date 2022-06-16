35c96a5ef874acf75d677466cfcbf06ca01c6737
Alba Castano <acastano@student.hive.fi>
2022-06-16 16:38:13 +0300

    changed to use static::resistsFire()

--- ex02/Targaryen.class.php
+++ ex02/Targaryen.class.php
@@ -6,7 +6,7 @@
         }
 
         public function getBurned() {
-            if ($this->resistsFire() === True)
+            if (static::resistsFire())
                 return "emerges naked but unharmed";
             else
                 return "burns alive";
