82ef92b7e51b4efc058619e23cd1ad4fa97d764d
Alba Castano <acastano@student.hive.fi>
2022-06-16 14:49:04 +0300

    erased everything else except family motto

--- ex01/Greyjoy.class.php
+++ ex01/Greyjoy.class.php
@@ -2,15 +2,5 @@
 
     class Greyjoy {
         protected $familyMotto = "We do not sow";
-
-        public function __construct()
-        {
-            return $this->familyMotto;
-        }
-
-        public function __destruct()
-        {
-            return ;
-        }
     }
 ?>
